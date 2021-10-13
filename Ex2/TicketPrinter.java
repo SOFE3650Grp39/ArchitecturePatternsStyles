031b797665c0bd90ed377f149710248ef6e4723b
RyzeNGrind <vidurshan.sribalasuhabiramam@uoit.net>
2021-10-12 22:43:52 -0400

    Update TicketPrinter.java

--- Ex2/TicketPrinter.java
+++ Ex2/TicketPrinter.java
@@ -1,4 +1,4 @@
-public class TicketPrinter {
+public class TicketPrinter implements ViewInterface {
            public void displayText(String text) {
                 System.out.println("------Ticket Printer------");
                 System.out.println(text);
