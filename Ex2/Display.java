95a7bd006b9f10eabc78f2a77831ff10c357a477
RyzeNGrind <vidurshan.sribalasuhabiramam@uoit.net>
2021-10-12 22:43:06 -0400

    Update Display.java

--- Ex2/Display.java
+++ Ex2/Display.java
@@ -1,4 +1,4 @@
-public class Display {
+public class Display implements ViewInterface {
 public void displayText(String text) {
                 System.out.println("------Display------");
                 System.out.println(text);
