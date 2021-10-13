ba39555f91a958f90b551cb4a12b39dde6dd7405
RyzeNGrind <vidurshan.sribalasuhabiramam@uoit.net>
2021-10-12 22:20:47 -0400

    Create CashRegister.java

---
+++ CashRegister.java
@@ -0,0 +1,22 @@
+public class CashRegister {
+private Display display = new Display();
+private TicketPrinter ticketPrinter = new TicketPrinter();
+private Product currentProduct;
+private ProductDB productDB = new ProductDB();
+private int UPCCode;
+
+public void setCurrentProductUPC(int UPCCode) {
+                this.UPCCode = UPCCode;
+                this.currentProduct = this.getCurrentProductInfo();
+
+                if (this.currentProduct != null) { // Prints only when the product is defined
+                        this.display.displayText(this.currentProduct.toString()); // Displays the scanned item
+                        this.ticketPrinter.displayText(this.currentProduct.toString());
+              }
+        }
+
+    public Product getCurrentProductInfo() {
+        currentProduct = this.productDB.GetProductInfo(this.UPCCode);
+        return currentProduct;
+    }
+}
