bba2dbadbbd2adfa5ec39e41bf85fa5378f9e4d1
RyzeNGrind <vidurshan.sribalasuhabiramam@uoit.net>
2021-10-12 22:25:06 -0400

    Create TestCashRegisterApp.java

---
+++ Ex2/TestCashRegisterApp.java
@@ -0,0 +1,9 @@
+public class TestCashRegisterApp {
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner();
+                Keyboard keyboard = new Keyboard();
+
+                scanner.scannedUPCCode(15324);
+                keyboard.setUPCCode(23254);
+        }
+}
